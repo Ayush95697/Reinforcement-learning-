cb5f84385928aa5d772c5a04b94f052932544f7b
Ayush <ayushmishra7548@gmail.com>
2026-04-02 17:00:20 +0530

    Custom env

---
+++ Snake_env.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+import gymnasium as gym
+from gymnasium import spaces
+from collections import deque
+SNAKE_LEN_GOAL=30
+
+import numpy as np
+import cv2
+import random
+import time
+
+
+def collision_with_apple(apple_position, score):
+    apple_position = [random.randrange(1, 50) * 10, random.randrange(1, 50) * 10]
+    score += 1
+    return apple_position, score
+
+
+def collision_with_boundaries(snake_head):
+    if snake_head[0] >= 500 or snake_head[0] < 0 or snake_head[1] >= 500 or snake_head[1] < 0:
+        return 1
+    else:
+        return 0
+class SnakeEnv(gym.Env):
+    """Custom Environment that follows gym interface"""
+
+    def __init__():
+        super(SnakeEnv, self).__init__()
+            # Define action and observation space
+            # They must be gym.spaces objects
+            # Example when using discrete actions:
+        self.action_space = spaces.Discrete(4)
+            # Example for using image as input (channel-first; channel-last also works):
+        self.observation_space = spaces.Box(low=0, high=255,
+                                            shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8)
+
+    def step(self, action):
+
+
+
+
+        info={}
+        return self.observation, self.reward, self.done, self.info
+
+
+
+
+    def reset(self):
+        self.done = False
+        self.img = np.zeros((500, 500, 3), dtype='uint8')
+        # Initial Snake and Apple position
+        self.snake_position = [[250, 250], [240, 250], [230, 250]]
+        self.apple_position = [random.randrange(1, 50) * 10, random.randrange(1, 50) * 10]
+        self.score = 0
+        self.prev_button_direction = 1
+        self.button_direction = 1
+        self.snake_head = [250, 250]
+
+        head_x = self.snake_head[0]
+        head_y = self.snake_head[1]
+
+        apple_delta_x = head_x - self.apple_position[0]
+        apple_delta_y = head_y - self.apple_position[1]
+        snake_length=len(self.snake_position)
+
+        self.prev_action = deque(maxlen=SNAKE_LEN_GOAL)
+
+        for _ in in range(SNAKE_LEN_GOAL):
+            self.prev_action(-1)
+        self.observation=[head_x, head_y, apple_delta_x, apple_delta_y, snake_length] + list(self.prev_action)
+        return self.observation  # reward, done, info can't be included
+
