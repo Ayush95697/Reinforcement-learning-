01618b0b817460c27ea9a0402a3a4131fe736aff
Ayush <ayushmishra7548@gmail.com>
2026-04-02 17:21:29 +0530

    ENV

--- Snake_env.py
+++ Snake_env.py
@@ -1,14 +1,12 @@
-from collections import deque
-
 import gymnasium as gym
 from gymnasium import spaces
-from collections import deque
-SNAKE_LEN_GOAL=30
-
 import numpy as np
 import cv2
 import random
 import time
+from collections import deque
+
+SNAKE_LEN_GOAL = 30
 
 
 def collision_with_apple(apple_position, score):
@@ -22,26 +20,36 @@ def collision_with_boundaries(snake_head):
         return 1
     else:
         return 0
-class SnakeEnv(gym.Env):
-    """Custom Environment that follows gym interface"""
 
-    def __init__():
-        super(SnakeEnv, self).__init__()
-            # Define action and observation space
-            # They must be gym.spaces objects
-            # Example when using discrete actions:
+
+def collision_with_self(snake_position):
+    snake_head = snake_position[0]
+    if snake_head in snake_position[1:]:
+        return 1
+    else:
+        return 0
+
+
+class SnekEnv(gym.Env):
+
+    def __init__(self):
+        super(SnekEnv, self).__init__()
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
         self.action_space = spaces.Discrete(4)
-            # Example for using image as input (channel-first; channel-last also works):
-        self.observation_space = spaces.Box(low=0, high=255,
-                                            shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8)
+        # Example for using image as input (channel-first; channel-last also works):
+        self.observation_space = spaces.Box(low=-500, high=500,
+                                            shape=(5 + SNAKE_LEN_GOAL,), dtype=np.float32)
 
     def step(self, action):
+        self.prev_actions.append(action)
         cv2.imshow('a', self.img)
         cv2.waitKey(1)
         self.img = np.zeros((500, 500, 3), dtype='uint8')
         # Display Apple
-        cv2.rectangle(self.img, (self.apple_position[0], self.apple_position[1]), (self.apple_position[0] + 10, self.apple_position[1] + 10),
-                      (0, 0, 255), 3)
+        cv2.rectangle(self.img, (self.apple_position[0], self.apple_position[1]),
+                      (self.apple_position[0] + 10, self.apple_position[1] + 10), (0, 0, 255), 3)
         # Display Snake
         for position in self.snake_position:
             cv2.rectangle(self.img, (position[0], position[1]), (position[0] + 10, position[1] + 10), (0, 255, 0), 3)
@@ -55,14 +63,15 @@ class SnakeEnv(gym.Env):
             else:
                 continue
 
+        button_direction = action
         # Change the head position based on the button direction
-        if action == 1:
+        if button_direction == 1:
             self.snake_head[0] += 10
-        elif action == 0:
+        elif button_direction == 0:
             self.snake_head[0] -= 10
-        elif action == 2:
+        elif button_direction == 2:
             self.snake_head[1] += 10
-        elif action == 3:
+        elif button_direction == 3:
             self.snake_head[1] -= 10
 
         # Increase Snake length on eating apple
@@ -78,35 +87,34 @@ class SnakeEnv(gym.Env):
         if collision_with_boundaries(self.snake_head) == 1 or collision_with_self(self.snake_position) == 1:
             font = cv2.FONT_HERSHEY_SIMPLEX
             self.img = np.zeros((500, 500, 3), dtype='uint8')
-            cv2.putText(self.img, 'Your Score is {}'.format(self.score), (140, 250), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(self.img, 'Your Score is {}'.format(self.score), (140, 250), font, 1, (255, 255, 255), 2,
+                        cv2.LINE_AA)
             cv2.imshow('a', self.img)
-            self.done=True
+            self.done = True
 
+        self.total_reward = len(self.snake_position) - 3  # default length is 3
+        self.reward = self.total_reward - self.prev_reward
+        self.prev_reward = self.total_reward
+
+        if self.done:
+            self.reward = -10
+        info = {}
 
         head_x = self.snake_head[0]
         head_y = self.snake_head[1]
 
-        apple_delta_x = head_x - self.apple_position[0]
-        apple_delta_y = head_y - self.apple_position[1]
-        snake_length=len(self.snake_position)
+        snake_length = len(self.snake_position)
+        apple_delta_x = self.apple_position[0] - head_x
+        apple_delta_y = self.apple_position[1] - head_y
 
-        self.prev_action = deque(maxlen=SNAKE_LEN_GOAL)
+        # create observation:
 
-        for _ in in range(SNAKE_LEN_GOAL):
-            self.prev_action(-1)
-        self.observation=[head_x, head_y, apple_delta_x, apple_delta_y, snake_length] + list(self.prev_action)
+        observation = [head_x, head_y, apple_delta_x, apple_delta_y, snake_length] + list(self.prev_actions)
+        observation = np.array(observation)
 
-
-
-
-        info={}
-        return self.observation, self.reward, self.done, self.info
-
-
-
+        return observation, self.reward, self.done, info
 
     def reset(self):
-        self.done = False
         self.img = np.zeros((500, 500, 3), dtype='uint8')
         # Initial Snake and Apple position
         self.snake_position = [[250, 250], [240, 250], [230, 250]]
@@ -116,17 +124,25 @@ class SnakeEnv(gym.Env):
         self.button_direction = 1
         self.snake_head = [250, 250]
 
+        self.prev_reward = 0
+
+        self.done = False
+
         head_x = self.snake_head[0]
         head_y = self.snake_head[1]
 
-        apple_delta_x = head_x - self.apple_position[0]
-        apple_delta_y = head_y - self.apple_position[1]
-        snake_length=len(self.snake_position)
+        snake_length = len(self.snake_position)
+        apple_delta_x = self.apple_position[0] - head_x
+        apple_delta_y = self.apple_position[1] - head_y
 
-        self.prev_action = deque(maxlen=SNAKE_LEN_GOAL)
+        self.prev_actions = deque(maxlen=SNAKE_LEN_GOAL)  # however long we aspire the snake to be
+        for i in range(SNAKE_LEN_GOAL):
+            self.prev_actions.append(-1)  # to create history
 
-        for _ in in range(SNAKE_LEN_GOAL):
-            self.prev_action(-1)
-        self.observation=[head_x, head_y, apple_delta_x, apple_delta_y, snake_length] + list(self.prev_action)
-        return self.observation  # reward, done, info can't be included
+        # create observation:
+        observation = [head_x, head_y, apple_delta_x, apple_delta_y, snake_length] + list(self.prev_actions)
+        observation = np.array(observation)
+
+        return observation
+
 
